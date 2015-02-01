2518c3f67a03a287784f366db823659837950788
hugo.alves <goninja@gmail.com>
2015-02-01 15:23:09 +0000

    criei pagina de teste com um painel para simular as tasks

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,8 @@ Rails.application.routes.draw do
 
   get 'instagram' => "insta#index"
 
+  get 'test' => "pages#test"
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
